25ee4c502ae4b64713bc7fd374f06d482fba3b66
RolandoD <r.delgado1203@gmail.com>
2018-09-11 04:56:50 -0700

    javascript refined, css inplemented, page styled as desired. product complete

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,44 +1,41 @@
 $(document).ready(function () {
-    var topics = ["World of Warcraft", "StarCraft", "Halo", "BioShock", "Super Smash Bros", "heroes of the storm"];
-
-    for (var i = 0; i < topics.length; i++) {
-        var btn = $("<button>");
-        btn.attr("value", topics[i]);
-        btn.text(topics[i]);
-        $("#gif-btns").append(btn);
-    }
-
-    $("#add-item").click(function () {
-        var newItem = $("#search-input").val();
-        var btn = $("<button>");
-        btn.attr("value", newItem);
-        btn.text(newItem);
-        $("#gif-btns").append(btn);
-        topics.push(newItem);
-        console.log(topics);
-    });
+    //initial array
+    var topics = ["Food", "Animals", "Video Games", "Movies", "Music", "Books"];
+    //load initial btn array on page
+    createBtns();
+    //event listener for adding new btns to the array
+    $("#add-item").on("click", addbtns);
+    //dynamic click handler for newly generated btns
+    $(document).on("click", "button", createGifs);
+    //dynamic click handler for gif start-stop
+    $(document).on("click", "img", startStop);
 
 
-
-    // start-stop gifs on click
-    $("body").on("click", "img", function () {
-        console.log(this);
-        var state = $(this).attr("data-state");
-        console.log(state);
-
-        if (state === "still") {
-            console.log('State was still');
-            $(this).attr("src", $(this).attr("data-animate"));
-            $(this).attr("data-state", "animate");
-        } else {
-            console.log('State was animated');
-            $(this).attr("src", $(this).attr("data-still"));
-            $(this).attr("data-state", "still");
+    //--------------------------------------------------functions----------------------------------------------------------//
+    //create clickable buttons on page
+    function createBtns() {
+        for (var i = 0; i < topics.length; i++) {
+            var btn = $("<button>");
+            btn.addClass("btn btn-outline-light")
+            btn.attr("value", topics[i]);
+            btn.text(topics[i]);
+            $("#gif-btns").append(btn);
         }
-    });
+    }
+    //add new btn to array, recreate btns displayed
+    function addbtns() {
+        var newItem = $("#search-input").val().trim();
+        if (newItem !== "") {
+            topics.push(newItem);
+            console.log(topics);
+            $("#gif-btns").empty();
+            createBtns();
+        }
+    }
+    //load gifs on page depending on the btn clicked
+    function createGifs() {
+        $("#gifs").empty();
 
-    // Adding click event listen listener to all buttons
-    $("body").on("click", "button", function () {
         var query = $(this).attr("value");
         var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=aBe1pDrA2XYrm6eGoGrqSPfSvTp8NviK&q=" +
             query + "&limit=10&offset=0&rating=PG-13&lang=en";
@@ -52,19 +49,37 @@ $(document).ready(function () {
 
             var results = response.data;
             for (var i = 0; i < results.length; i++) {
-                var gifDiv = $("<div>");                
-                var p = $("<p>").text("Rating: " + results[i].rating);
+                var gifDiv = $("<div>");
+                gifDiv.addClass("individual-gif text-center");
+                var textDiv = $("<div>").text("Rating: " + results[i].rating.toUpperCase());
+                textDiv.addClass("card-body");
                 var gif = $("<img>");
-                gif.attr("class", "gif");
-                gif.attr("src", results[i].images.fixed_height_still.url);
-                gif.attr("data-still", results[i].images.fixed_height_still.url);                
-                gif.attr("data-animate", results[i].images.fixed_height.url)
+                gif.addClass("card gif");
+                gif.attr("src", results[i].images.fixed_width_still.url);
+                gif.attr("data-still", results[i].images.fixed_width_still.url);
+                gif.attr("data-animate", results[i].images.fixed_width.url)
                 gif.attr("data-state", "still");
                 gifDiv.append(gif);
-                gifDiv.append(p);                
+                gifDiv.append(textDiv);
                 $("#gifs").prepend(gifDiv);
             }
         });
-    });
+    }
+    //start-stop gif aimations
+    function startStop() {
+        console.log(this);
+        var state = $(this).attr("data-state");
+        console.log(state);
+
+        if (state === "still") {
+            console.log('State was still');
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+        } else {
+            console.log('State was animated');
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+        }
+    }
 
 });
